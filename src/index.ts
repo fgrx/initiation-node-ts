9514dfc4afc016dfcd4631bacce4358a398b4b13
Fabien Grignoux <fabien.grignoux@outlook.com>
2023-04-19 10:32:58 +0200

    Refactorisation : affichage des stats dans le layout

--- src/index.ts
+++ src/index.ts
@@ -3,7 +3,11 @@ import {
   getFilesContentInJson,
   getFilesInFolder,
 } from "./services/dataProvider";
-import { displayCloseMessage, displayWelcomeMessage } from "./services/layout";
+import {
+  displayAnalytics,
+  displayCloseMessage,
+  displayWelcomeMessage,
+} from "./services/layout";
 
 displayWelcomeMessage("Rapport sur les employés", "Ma petite entreprise");
 
@@ -15,14 +19,6 @@ const filesContent = getFilesContentInJson(employeesFolder, files);
 
 const analytics = new Analytics(filesContent);
 
-console.log(`\n Nombre d'employés : ${analytics.getNumberOfEmployees()}`);
-console.log(`\n Salaire total des employés : ${analytics.getTotalSalaries()}`);
-
-const { inTrialPeriod, notInTrialPeriod } =
-  analytics.getEmployeesInTrialPeriodOrNot();
-console.log(
-  `\n Nombre d'employés en période d'essai :  ${inTrialPeriod.length}`
-);
-console.log(`\n Nombre d'employés titulaires :  ${notInTrialPeriod.length}`);
+displayAnalytics(analytics);
 
 displayCloseMessage("Fin du rapport");
--- src/services/layout.ts
+++ src/services/layout.ts
@@ -1,3 +1,5 @@
+import Analytics from "./analytics";
+
 function displayWelcomeMessage(message: string, companyName: string): void {
   const dateObject = new Date();
   const todayDate = dateObject.toLocaleDateString();
@@ -15,4 +17,18 @@ const displayCloseMessage = function (message: string): void {
   );
 };
 
-export { displayWelcomeMessage, displayCloseMessage };
+const displayAnalytics = (analytics: Analytics): void => {
+  console.log(`\n Nombre d'employés : ${analytics.getNumberOfEmployees()}`);
+  console.log(
+    `\n Salaire total des employés : ${analytics.getTotalSalaries()}`
+  );
+
+  const { inTrialPeriod, notInTrialPeriod } =
+    analytics.getEmployeesInTrialPeriodOrNot();
+  console.log(
+    `\n Nombre d'employés en période d'essai :  ${inTrialPeriod.length}`
+  );
+  console.log(`\n Nombre d'employés titulaires :  ${notInTrialPeriod.length}`);
+};
+
+export { displayWelcomeMessage, displayCloseMessage, displayAnalytics };
