808c4abb5a247c49ded1bfafbea149824183d21e
Fabien Grignoux <fabien.grignoux@outlook.com>
2023-04-19 10:06:11 +0200

    Récupération des données des employés

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,16 @@
-import { getFilesInFolder } from "./service/dataProvider";
+import {
+  getFilesContentInJson,
+  getFilesInFolder,
+} from "./service/dataProvider";
 import { displayCloseMessage, displayWelcomeMessage } from "./service/layout";
 
 displayWelcomeMessage("Rapport sur les employés", "Ma petite entreprise");
 
 // Affichage du rapport
-getFilesInFolder("./employees");
+const employeesFolder = "./employees";
+
+const files = getFilesInFolder(employeesFolder);
+const filesContent = getFilesContentInJson(employeesFolder, files);
+console.log(filesContent);
 
 displayCloseMessage("Fin du rapport");
--- src/service/dataProvider.ts
+++ src/service/dataProvider.ts
@@ -3,7 +3,6 @@ import fs from "fs";
 const getFilesInFolder = (folder: string): string[] => {
   try {
     const files = fs.readdirSync(`./${folder}`);
-    console.log(files);
     return files;
   } catch (error) {
     console.error("Erreur : pas de dossier trouvé", error);
@@ -11,4 +10,26 @@ const getFilesInFolder = (folder: string): string[] => {
   }
 };
 
-export { getFilesInFolder };
+function getFilesContentInJson(folder: string, files: string[]): any[] {
+  const employeesData: any[] = [];
+
+  files.forEach((file) => {
+    const path = completeFilePath(folder, file);
+
+    try {
+      const content = fs.readFileSync(path, "utf8");
+      const contentInJSON = JSON.parse(content);
+      console.log(contentInJSON);
+
+      employeesData.push(contentInJSON);
+    } catch (error) {
+      console.error(`${path} : Problème avec le fichier`, error);
+    }
+  });
+  return employeesData;
+}
+
+const completeFilePath = (folder: string, file: string) =>
+  `./${folder}/${file}`;
+
+export { getFilesInFolder, getFilesContentInJson };
